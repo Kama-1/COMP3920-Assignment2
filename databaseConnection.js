4b134d45c32fce39df1f639ca2a80a2ca6e7f58d
Anna Jang <aj3spencer@gmail.com>
2026-03-22 14:36:29 -0700

    feat: connection to databaes

---
+++ databaseConnection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2/promise');
+
+const dbConfig = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    multipleStatements: false
+};
+
+var database = mysql.createPool(dbConfig);
+
+module.exports = database;
--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+//Define the include function for absolute file name
+global.base_dir = __dirname;
+global.abs_path = function(path) {
+    return base_dir + path;
+}
+global.include = function(file) {
+    return require(abs_path('/' + file));
+}
+
 require('dotenv').config();
 
 const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
-
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+
+const database = include('databaseConnection');
+const db_utils = include('database/db_utils');
+const success = db_utils.printMySQLVersion();
+
 
 app.get('/', (req, res) => {
     res.render("landing") // Send to views/landing
