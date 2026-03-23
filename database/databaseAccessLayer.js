a53e52fbb19752e55800fa7a0c80bb8a4629aa94
Anna Jang <aj3spencer@gmail.com>
2026-03-22 22:07:56 -0700

    feat: added new functions to databaseAccessLayer

--- database/databaseAccessLayer.js
+++ database/databaseAccessLayer.js
@@ -2,8 +2,8 @@ const database = include('database/databaseConnection');
 
 async function getAllUsers() {
     let sqlQuery = `
-		SELECT web_user_id, first_name, last_name, email
-		FROM web_user;
+		SELECT user_id, username, password
+		FROM user;
 	`;
 
     try {
@@ -12,7 +12,26 @@ async function getAllUsers() {
         return results[0];
     }
     catch (err) {
-        console.log("Error selecting from todo table");
+        console.log("Error selecting from users table");
+        console.log(err);
+        return null;
+    }
+}
+
+async function getUserById(userID) {
+    let sqlQuery = `
+		SELECT user_id, username, password
+		FROM user
+		WHERE user_id = ${userID};
+	`;
+
+    try {
+        const results = await database.query(sqlQuery);
+        console.log(results[0]);
+        return results[0];
+    }
+    catch (err) {
+        console.log(`Error selecting specific user ${userID}`);
         console.log(err);
         return null;
     }
@@ -20,15 +39,12 @@ async function getAllUsers() {
 
 async function addUser(postData) {
     let sqlInsert = `
-		INSERT INTO web_user (first_name, last_name, email, password_hash) 
-		VALUES (:first_name, :last_name, :email, :password_hash);
+		INSERT INTO user (username, password) 
+		VALUES (:username, :password);
 	`;
-    let passwordHash = bcrypt.hashSync(postData.password, 12);
     let params = {
-        first_name: postData.first_name,
-        last_name: postData.last_name,
-        email: postData.email,
-        password_hash: passwordHash
+        username: postData.username,
+        password: postData.password
     };
     console.log(sqlInsert);
     try {
@@ -45,5 +61,25 @@ async function addUser(postData) {
     }
 }
 
+async function getAllGroupsForUserByID(userID) {
+    let sqlQuery = `
+		SELECT room_id, name
+        FROM room
+                 JOIN user_room ON user_room.room_id = room.room_id
+                 JOIN user ON user.user_id = user_room.user_id
+        WHERE user.user_id = ${userID};
+	`;
 
-module.exports = {getAllUsers, addUser}
+    try {
+        const results = await database.query(sqlQuery);
+        console.log(results[0]);
+        return results[0];
+    }
+    catch (err) {
+        console.log(`Error selecting rooms for user ${userID}`);
+        console.log(err);
+        return null;
+    }
+}
+
+module.exports = {getAllUsers, addUser, getUserById, getAllGroupsForUserByID}
