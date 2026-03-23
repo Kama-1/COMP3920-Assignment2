6f5294779d012f25912f02d442dadb5bfe2ba7db
Anna Jang <aj3spencer@gmail.com>
2026-03-22 21:50:09 -0700

    fix: undid previous fix

---
+++ database/databaseAccessLayer.js
@@ -0,0 +1,49 @@
+const database = include('database/databaseConnection');
+
+async function getAllUsers() {
+    let sqlQuery = `
+		SELECT web_user_id, first_name, last_name, email
+		FROM web_user;
+	`;
+
+    try {
+        const results = await database.query(sqlQuery);
+        console.log(results[0]);
+        return results[0];
+    }
+    catch (err) {
+        console.log("Error selecting from todo table");
+        console.log(err);
+        return null;
+    }
+}
+
+async function addUser(postData) {
+    let sqlInsert = `
+		INSERT INTO web_user (first_name, last_name, email, password_hash) 
+		VALUES (:first_name, :last_name, :email, :password_hash);
+	`;
+    let passwordHash = bcrypt.hashSync(postData.password, 12);
+    let params = {
+        first_name: postData.first_name,
+        last_name: postData.last_name,
+        email: postData.email,
+        password_hash: passwordHash
+    };
+    console.log(sqlInsert);
+    try {
+        const results = await database.query(sqlInsert, params);
+        console.log({results});
+        console.log(`Inserted new row with id: ${results[0].insertId}`);
+
+        return true;
+    }
+    catch (err) {
+        console.log(err);
+
+        return false;
+    }
+}
+
+
+module.exports = {getAllUsers, addUser}
--- database/db_utils.js
+++ database/db_utils.js
@@ -9,12 +9,12 @@ async function printMySQLVersion() {
         const results = await database.query(sqlQuery);
         console.log("Successfully connected to MySQL");
         console.log(results[0]);
-        return Promise.resolve(200);
+        return true
     }
     catch(err) {
         console.log("Error getting version from MySQL");
         console.log(err);
-        return Promise.reject(new Error(400));
+        return false;
     }
 }
 
