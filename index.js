c75bba77e58c7f25777669b123ffeeb0304c992d
Anna Jang <aj3spencer@gmail.com>
2026-03-25 00:53:35 -0700

    feat: emojis displayed crudely

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 const dbUtils = require('./database/db_utils');
 const bcrypt = require('bcrypt');
 const saltRounds = 12;
